b03be786e688f4c82174973e142fc051c053f6b3
theoske <urufudjd@gmail.com>
2022-04-22 21:12:58 +0200

    maj

--- pipex.c
+++ pipex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkempf-e <tkempf-e@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: theo <theo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 14:38:34 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/04/20 16:37:54 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/04/22 21:12:04 by theo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,31 @@ resultat de la cmd1 est l'entree de cmd2.
 resultat de la deuxieme est entree dans file2.
 */
 
+/*
+void perror(const char *str) : affiche le message d'erreur a la suite de str.
+
+char *strerror(int errnum) : renvoie l'erreur dans une chaine. Prend le numero de l'erreur en parametre (en general errno).
+
+int access(const char *pathname, int mode) : verifie l'acces a un fichier avec mode au choix (F/R/W/X_OK). return 0 if success. 
+-1 if failure and sets errno correctly.
+
+int dup(int oldfd) : return new file descriptor.
+
+int dup2(int oldfd, int newfd) : comme dup mais au lieu d'utiliser le fd du numero le plus bas, il utilise celui
+de numero newfd.
+
+int execve(const char *filepath, char *const argv[], char *const envp[]) : execute le programme de filepath.
+argv sont les arguments a passer au programme. envp sont les variables d'env, se termine par NULL.
+Ne retourne rien en cas de succes.
+
+pid_t fork(void) : cree un nouveau processus (=child) en dupliquant celui qui est appelé (=parent).
+child has unique process ID.
+
+int pipe(int pipefd[2]) : cree un tunnel unidirectionnel de données permettant une communication inter-processus. 
+pipe[0] est le fd de lecture et pipe[1] le fd d'ecriture. 0 retourner en cas de succes.
+
+int unlink(const char *pathname) : supprime un nom du systeme de fichier
+*/
 #include <unistd.h>
 #include <stdio.h>
 
