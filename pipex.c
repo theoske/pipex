075e6296d495fafe6f7c0a6a006ff69c1d1aca4e
Theo Kempf elbaz <tkempf-e@k0r3p4.42mulhouse.fr>
2022-04-26 19:07:37 +0200

    X cmd

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 14:38:34 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/04/26 17:04:34 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/04/26 19:06:53 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,11 +63,184 @@ int unlink(const char *pathname) : supprime un nom du systeme de fichier
 */
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 //tester chaque path avec access et lancer cmd avec celui aui fonctionne
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static int	ft_nbwords(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	nb_word;
+
+	if (s[0] == '\0')
+		return (0);
+	i = 0;
+	while (s[i] == c && s[i])
+		i++;
+	nb_word = 0;
+	while (s[i])
+	{
+		if (s[i] && s[i] == c && s[i - 1] != c)
+			nb_word++;
+		i++;
+	}
+	if (s[i] == '\0' && s[i - 1] != c)
+		nb_word++;
+	return (nb_word);
+}
+
+static char	**ft_tab(char	**tab, char	const *s, char c, int nb_word)
+{
+	int	i;
+	int	j;
+	int	compt;
+
+	i = 0;
+	j = 0;
+	while (s[i] && j < nb_word)
+	{
+		while (s[i] == c && s[i])
+			i++;
+		compt = 0;
+		while (s[i] != c && s[i++])
+			compt ++;
+		if (s[i - 1] != c)
+		{
+			tab[j] = malloc(sizeof(char) * (compt + 1));
+			if (!tab)
+				return (0);
+			tab[j][compt] = '\0';
+		}
+		j++;
+	}
+	return (tab);
+}
+
+static char	**ft_malloc_tab2d(char const *s, char c)
+{
+	size_t			nb_word;
+	char			**tab;
+
+	nb_word = ft_nbwords(s, c);
+	if (s[0] == '\0' && nb_word == 0)
+	{
+		tab = (char **)malloc(sizeof(char *));
+		tab[0] = NULL;
+		return (tab);
+	}
+	tab = (char **)malloc(sizeof(char *) * (nb_word + 1));
+	if (!tab)
+		return (0);
+	tab[nb_word] = 0;
+	tab = ft_tab(tab, s, c, nb_word);
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	index[2];
+	char			**tab;
+
+	tab = ft_malloc_tab2d(s, c);
+	if (!tab)
+		return (NULL);
+	i = 0;
+	index[0] = 0;
+	while (tab[index[0]] && s[i])
+	{
+		while (s[i] == c && s[i])
+			i++;
+		index[1] = 0;
+		while (s[i] != c && s[i])
+		{
+			tab[index[0]][index[1]] = s[i];
+			index[1]++;
+			i++;
+		}
+		index[0]++;
+	}
+	return (tab);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	join = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		join[i] = *(char *)(s1 + i);
+		i++;
+	}
+	while (s2[j])
+	{
+		join[i + j] = *(char *)(s2 + j);
+		j++;
+	}
+	join[i + j] = '\0';
+	// free(s1);
+	return (join);
+}
+
+char	*ft_env(char **envp)
+{
+	int		i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T'
+		&& envp[i][3] == 'H')
+			return (envp[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_path_tester(char *totest, char *cmd)
+{
+	char	**tab;
+	int		i;
+
+	i = 0;
+	tab = ft_split(totest, ':');
+	while (tab && tab[i])
+	{
+		tab[i] = ft_strjoin(tab[i], cmd);
+		if (access(tab[i], R_OK) == 0)
+			return (tab[i]);
+		i++;
+	}
+	return (NULL);
+}
+
 int	main(int argc, const char **argv, char **envp)
 {
-	char *cmd[] = {"ls", NULL};
-	execve("/bin/ls", cmd, envp);
+	char *path;
+
+	path = ft_env(envp);
+	path = ft_path_tester(path, ft_strjoin("/", (char *)argv[2]));
+	// printf("\n\n%s\n\n", argv[2]);
+	// printf("%s\n\n", path);
+	execve(path, &argv[2], envp);
 	return (0);
 }
